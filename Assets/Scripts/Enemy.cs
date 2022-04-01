fc686451732a58dfa1a3a1586ea1b6065adb0ba1
Robert <robertjwisniowski@gmail.com>
2022-04-01 22:29:43 +0200

    Sounds added.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,6 +12,10 @@ public class Enemy : MonoBehaviour
     [SerializeField] float maxTimeBetweenShots = 3f;
     [SerializeField] GameObject deathVFX;
     [SerializeField] float durationOfExplosion = 1f;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] [Range(0,1)] float deathSoundVolume = 0.7f;
+    [SerializeField] AudioClip shootSound;
+    [SerializeField][Range(0, 1)] float shootSoundVolume = 0.25f;
 
     void Start()
     {
@@ -37,6 +41,7 @@ public class Enemy : MonoBehaviour
     {
         GameObject laser = Instantiate(projectile,
             transform.position, Quaternion.identity) as GameObject;
+        AudioSource.PlayClipAtPoint(shootSound, Camera.main.transform.position, shootSoundVolume);
         laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -ProjectileSpeed);
     }
 
@@ -65,5 +70,6 @@ public class Enemy : MonoBehaviour
         Destroy(gameObject);
         GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
         Destroy(explosion, durationOfExplosion);
+        AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,10 @@ public class Player : MonoBehaviour
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float ProjectileSpeed = 10f;
     [SerializeField] float projectileFiringPeriod = 2f;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField][Range(0, 1)] float deathSoundVolume = 0.7f;
+    [SerializeField] AudioClip shootSound;
+    [SerializeField][Range(0, 1)] float shootSoundVolume = 0.25f;
     Coroutine firingCoroutine;
     float horizontalMove = 0f;
     float verticalMove = 0f;
@@ -48,6 +52,7 @@ public class Player : MonoBehaviour
             GameObject laser = Instantiate(laserPrefab,
                 transform.position, Quaternion.identity) as GameObject;
             laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, ProjectileSpeed);
+            AudioSource.PlayClipAtPoint(shootSound, Camera.main.transform.position, shootSoundVolume);
             yield return new WaitForSeconds(projectileFiringPeriod);
         }
     }
@@ -80,8 +85,14 @@ public class Player : MonoBehaviour
         damageDealer.Hit();
         if (health < 0)
         {
-            Destroy(gameObject);
+            Die();
         }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume);
     }
 
     private void SetUpMoveBoundaries()
