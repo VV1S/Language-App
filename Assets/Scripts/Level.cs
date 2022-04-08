0126bff55bba1de5630c76721c3b3c466bf93d23
Robert <robertjwisniowski@gmail.com>
2022-04-08 23:02:18 +0200

    Score display added.

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -16,6 +16,7 @@ public class Level : MonoBehaviour
     public void LoadGame()
     {
         SceneManager.LoadScene(1);
+        FindObjectOfType<GameSession>()?.ResetGame();
     }
 
     public void LoadGameOver()
